3f553e837091a7c5deea0aa26b8ffa170e5012d9
OOP <opkampitak@hotmail.com>
2016-12-23 00:08:12 +0700

    Use JavaScript Standard Style

--- isolate.js
+++ isolate.js
@@ -1,15 +1,11 @@
-const cp=require('child_process')
-function isolate(args){
-    return new Promise((res,rej)=>{
-        cp.execFile('/usr/local/bin/isolate',args,(err,stdout,stderr)=>{
-            if(err){
-                console.log("ERR")
-                console.log(stdout)
-                console.log(stderr)
-                rej(err)
-            }
-            else res({stdout:stdout,stderr:stderr})
-        })
+const cp = require('child_process')
+function isolate (args) {
+  return new Promise((resolve, reject) => {
+    cp.execFile('/usr/local/bin/isolate', args, (err, stdout, stderr) => {
+      if (err) {
+        reject(err)
+      } else resolve({stdout: stdout, stderr: stderr})
     })
+  })
 }
-module.exports=isolate
+module.exports = isolate
